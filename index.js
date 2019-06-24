76301f09d75b6d4e04ecbc9e85dc6fea0844a153
Amy Cen <amy.cen@blackrock.com>
2019-06-24 15:52:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
-document.getElementById("main").remove();
-let newHeader = document.createElement("h1#victory")
+document.getElementById("main").remove()
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.innerHTML= `YOUR-NAME is the champion`;
 document.body.appendChild(newHeader)
