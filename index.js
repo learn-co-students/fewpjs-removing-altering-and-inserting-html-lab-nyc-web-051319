b25449517241e65c40d1e8d53c10ee3fa877041b
locky5 <daikevin19@yahoo.com>
2019-06-24 11:14:23 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code here!
+
+main.remove();
+
+const newHeader = document.createElement("h1"); //creates new header of h1
+newHeader.id = "victory" //gives id to header
+newHeader.innerHTML = "Flatiron School is the champion!"; //prints HTML to this header
+newHeader.className = "victory"; //gives classname to header
